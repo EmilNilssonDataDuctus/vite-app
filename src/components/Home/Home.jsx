22f281111430b569a54a9bfe7863eb0c7539d892
Emil Nilsson <emil.nilsson@dataductus.se>
2023-07-03 14:25:07 +0200

    Remove unused code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,11 @@
 import { useState } from "react";
 import "./Home.css";
 
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
-
 export function Home() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -26,9 +15,6 @@ export function Home() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
