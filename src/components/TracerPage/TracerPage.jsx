c62b34cfaa54c4f7ce53758751bf0c3b18906894
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-10 17:10:19 +0200

    Add smaller version of the same idea

--- src/components/TracerPage/TracerPage.jsx
+++ src/components/TracerPage/TracerPage.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import { MainWrapper } from "../../Shared/Page.styled";
 import {
   DisplayContainer,
+  DisplayContainerSmall,
   DisplayContainerWrapper,
   ShowDiv,
+  ShowDivSmall,
 } from "./TracerPage.styled";
 
 export const TracerPage = () => {
   const [leftShift, setLeftShift] = useState(0);
   const [topShift, setTopShift] = useState(0);
+
+  const [leftShiftSmall, setLeftShiftSmall] = useState(0);
+  const [topShiftSmall, setTopShiftSmall] = useState(0);
 
   const detectMouse = (e) => {
     const offsetX = e.target.getBoundingClientRect().x;
@@ -19,6 +24,12 @@ export const TracerPage = () => {
 
     setLeftShift(() => coordX + "px");
     setTopShift(() => coordY + "px");
+
+    const coordXSmall = Math.floor((e.clientX - offsetX) / 2);
+    const coordYSmall = Math.floor((e.clientY - offsetY) / 2);
+
+    setLeftShiftSmall(() => coordXSmall + "px");
+    setTopShiftSmall(() => coordYSmall + "px");
   };
 
   return (
@@ -28,6 +39,12 @@ export const TracerPage = () => {
         <h2>Opaque div on div generator</h2>
         <DisplayContainerWrapper>
           <DisplayContainer onMouseMove={detectMouse}></DisplayContainer>
+          <DisplayContainerSmall>
+            <ShowDivSmall
+              $left={leftShiftSmall}
+              $top={topShiftSmall}
+            ></ShowDivSmall>
+          </DisplayContainerSmall>
           <DisplayContainer>
             <ShowDiv $left={leftShift} $top={topShift}></ShowDiv>
           </DisplayContainer>
--- src/components/TracerPage/TracerPage.styled.js
+++ src/components/TracerPage/TracerPage.styled.js
@@ -1,5 +1,12 @@
 import { styled } from "styled-components";
 
+export const DisplayContainerWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  gap: 16px;
+`;
+
 export const DisplayContainer = styled.div`
   width: 600px;
   height: 600px;
@@ -15,9 +22,17 @@ export const ShowDiv = styled.div`
   top: ${(props) => props.$top};
 `;
 
-export const DisplayContainerWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 16px;
+export const DisplayContainerSmall = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+export const ShowDivSmall = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: rgba(0, 0, 0, 0.4);
+  position: relative;
+  left: ${(props) => props.$left};
+  top: ${(props) => props.$top};
 `;
