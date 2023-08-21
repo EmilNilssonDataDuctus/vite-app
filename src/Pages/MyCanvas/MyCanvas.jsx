6603b17865c6f0dbf7339770cff932d690a8fa59
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-21 12:56:57 +0200

    First commit canvaspage

---
+++ src/Pages/MyCanvas/MyCanvas.jsx
@@ -0,0 +1,10 @@
+import { MainWrapper } from "../../Shared/Page.styled";
+
+export const MyCanvas = () => {
+  return (
+    <MainWrapper>
+      <h1>My canvas</h1>
+      <canvas id="myPaper"></canvas>
+    </MainWrapper>
+  );
+};
