823bd9e07ce04d166d93415011d268563051829b
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-28 15:28:48 +0200

    Save history of value of the state in new state

--- src/Pages/CodeAlong/CodeAlong.tsx
+++ src/Pages/CodeAlong/CodeAlong.tsx
@@ -2,17 +2,33 @@ import React, { useState } from "react";
 import { MainWrapper } from "../../Shared/Page.styled";
 
 export const CodeAlong = () => {
+  const [history, setHistory] = useState<string[]>([]);
   const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    setHistory([e.target.value, ...history]);
+  };
 
   return (
     <MainWrapper>
       <h1>Code along with pluralsight</h1>
       <input
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={(e) => handleChange(e)}
         placeholder="Enter some text"
       />
       <p>{value}</p>
+      {history && history.length > 0 && (
+        <div>
+          History:
+          <ul>
+            {history.map((entry) => (
+              <li>{entry}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </MainWrapper>
   );
 };
