5607c905a7a02bec47ceb3ed1066c25033139dc2
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-28 16:08:40 +0200

    Showcase usecase for react useRef

--- src/Pages/CodeAlong/CodeAlong.tsx
+++ src/Pages/CodeAlong/CodeAlong.tsx
@@ -1,4 +1,9 @@
-import React, { useState } from "react";
+import leopardBw from "/leopard-bw.jpg";
+import leopard from "/leopard.jpg";
+import mountainBw from "/mountain-bw.jpg";
+import mountain from "/mountain.jpg";
+
+import React, { useRef, useState } from "react";
 import { MainWrapper } from "../../Shared/Page.styled";
 
 export const CodeAlong = () => {
@@ -13,22 +18,60 @@ export const CodeAlong = () => {
   return (
     <MainWrapper>
       <h1>Code along with pluralsight</h1>
-      <input
-        value={value}
-        onChange={(e) => handleChange(e)}
-        placeholder="Enter some text"
-      />
-      <p>{value}</p>
-      {history && history.length > 0 && (
-        <div>
-          History:
-          <ul>
-            {history.map((entry) => (
-              <li>{entry}</li>
-            ))}
-          </ul>
+      <section>
+        <h2>Usestate hook</h2>
+        <input
+          value={value}
+          onChange={(e) => handleChange(e)}
+          placeholder="Enter some text"
+        />
+        <p>{value}</p>
+        {history && history.length > 0 && (
+          <div>
+            History:
+            <ul>
+              {history.map((entry) => (
+                <li>{entry}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </section>
+      <section>
+        <h2>Useref hook</h2>
+        <div style={{ display: "flex", gap: "16px", margin: "32px" }}>
+          <div>
+            <ImageToggleOnMouseOver
+              primaryImg={leopard}
+              secondaryImg={leopardBw}
+            />
+          </div>
+          <div>
+            <ImageToggleOnMouseOver
+              primaryImg={mountain}
+              secondaryImg={mountainBw}
+            />
+          </div>
         </div>
-      )}
+      </section>
     </MainWrapper>
+  );
+};
+
+const ImageToggleOnMouseOver = ({ primaryImg = "", secondaryImg = "" }) => {
+  const imageRef: null | any = useRef(null);
+  return (
+    <>
+      <img
+        ref={imageRef}
+        src={primaryImg}
+        onMouseOver={() => {
+          imageRef.current.src = primaryImg;
+        }}
+        onMouseOut={() => {
+          imageRef.current.src = secondaryImg;
+        }}
+      />
+    </>
   );
 };
