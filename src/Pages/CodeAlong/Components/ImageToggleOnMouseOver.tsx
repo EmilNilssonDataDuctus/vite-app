49b1e38a6b6f3b333d4facf1e362894e631bfdf9
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-29 13:22:29 +0200

    Attempt to showcase eslint rules

--- src/Pages/CodeAlong/Components/ImageToggleOnMouseOver.tsx
+++ src/Pages/CodeAlong/Components/ImageToggleOnMouseOver.tsx
@@ -1,22 +1,44 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+const randomd = Math.random() >= 0.5;
 
 export const ImageToggleOnMouseOver = ({
   primaryImg = "",
   secondaryImg = "",
 }) => {
   const imageRef: null | any = useRef(null);
+  // OK
+  const [t, a] = useState(randomd === true);
+
+  // Not OK
+  // if (randomd) {
+  //   const [t, a] = useState(true);
+  // } else {
+  //   const [t, a] = useState(false);
+  // }
+
+  useEffect(() => {
+    setTimeout(() => {
+      a(false);
+    }, 2000);
+  });
+
   return (
     <>
-      <img
-        ref={imageRef}
-        src={primaryImg}
-        onMouseOver={() => {
-          imageRef.current.src = primaryImg;
-        }}
-        onMouseOut={() => {
-          imageRef.current.src = secondaryImg;
-        }}
-      />
+      {t ? (
+        <div>Loading...</div>
+      ) : (
+        <img
+          ref={imageRef}
+          src={primaryImg}
+          onMouseOver={() => {
+            imageRef.current.src = primaryImg;
+          }}
+          onMouseOut={() => {
+            imageRef.current.src = secondaryImg;
+          }}
+        />
+      )}
     </>
   );
 };
