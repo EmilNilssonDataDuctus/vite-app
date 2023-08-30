66e56e62a0f7f5e77e6bbc2c36c8b86d01b01f6c
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-30 11:54:49 +0200

    Showcase the array function some by displaying motivational messages based on progress

--- src/Pages/TodoPage/TodoPage.tsx
+++ src/Pages/TodoPage/TodoPage.tsx
@@ -43,6 +43,29 @@ export const TodoPage = () => {
   return (
     <MainWrapper>
       <h1>Todo list</h1>
+      {todos.some((todo) => !todo.completed) ? (
+        <>
+          <p>
+            You still have todos to check off!
+            {todos.some((todo) => todo.completed) ? (
+              <>
+                {" "}
+                <span>
+                  You are well on your way to complete your list off todos, keep
+                  up the good work!
+                </span>
+              </>
+            ) : (
+              <>
+                {" "}
+                <span>You have yet to begin completing your tasks</span>
+              </>
+            )}
+          </p>
+        </>
+      ) : (
+        <p>You have completed all your todos</p>
+      )}
       <ul>
         {todos.map((todo) => (
           <TodoComponent
