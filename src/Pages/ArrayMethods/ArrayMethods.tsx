9c6edb870b23b18a6a5793fb78f3c5c700d701ab
Emil Nilsson <emil.nilsson@dataductus.se>
2023-09-05 16:30:07 +0200

    Add game with objective to get 3 colors in row out of 4 colors possible

---
+++ src/Pages/ArrayMethods/ArrayMethods.tsx
@@ -0,0 +1,193 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { MainWrapper } from "../../Shared/Page.styled";
+import { getRandomIntInclusive } from "../MemoryGame/MemoryGame";
+import {
+  BeadHolder,
+  ColoredBead,
+  Container,
+  ControlCenter,
+  GameArea,
+} from "./ArrayMethods.styled";
+
+const initialState = [
+  {
+    id: 1,
+    color: "yellow",
+  },
+  {
+    id: 2,
+    color: "yellow",
+  },
+  {
+    id: 3,
+    color: "blue",
+  },
+  {
+    id: 4,
+    color: "blue",
+  },
+  {
+    id: 5,
+    color: "yellow",
+  },
+  {
+    id: 6,
+    color: "red",
+  },
+];
+
+const myReducer = (state, action) => {
+  console.log("state: ", state);
+
+  switch (action.type) {
+    case "SET_TODOS": {
+      return action.payload;
+    }
+
+    case "ADD_TO_LEFT": {
+      return [action.payload, ...state];
+    }
+
+    case "ADD_TO_RIGHT": {
+      return [...state, action.payload];
+    }
+
+    case "REMOVE_FROM_LEFT": {
+      return [...state].filter((_, index) => index !== 0);
+    }
+
+    case "REMOVE_FROM_RIGHT": {
+      return [...state].filter((_, index, array) => index !== array.length - 1);
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+const COLORS = ["red", "green", "blue", "yellow"];
+
+export const ArrayMethods = () => {
+  const [beads, dispatch] = useReducer(myReducer, initialState);
+
+  const [nextBeadColor, setNextBeadColor] = useState("yellow");
+  const [score, setScore] = useState(0);
+
+  const getNextBeadColor = () =>
+    COLORS[getRandomIntInclusive(0, COLORS.length - 1)];
+
+  const addToLeft = () => {
+    dispatch({
+      type: "ADD_TO_LEFT",
+      payload: { id: uuidv4(), color: nextBeadColor },
+    });
+    setNextBeadColor(getNextBeadColor());
+  };
+
+  const addToRight = () => {
+    dispatch({
+      type: "ADD_TO_RIGHT",
+      payload: { id: uuidv4(), color: nextBeadColor },
+    });
+    setNextBeadColor(getNextBeadColor());
+  };
+
+  const removeFromLeft = () => {
+    dispatch({
+      type: "REMOVE_FROM_LEFT",
+    });
+  };
+
+  const removeFromRight = () => {
+    dispatch({
+      type: "REMOVE_FROM_RIGHT",
+    });
+  };
+
+  useEffect(() => {
+    console.log("beads: ", beads);
+
+    // const firstThreeBeadsAreTheSame = (beads) => {
+    //   if (beads[0].color === beads[2].color) {
+    //     console.log("beads need to be removed: ");
+    //     return true;
+    //   }
+    //   console.log("beads have been removed: ");
+
+    //   return false;
+    // };
+
+    const firstThreeBeadsAreTheSame = (beads) => {
+      if (
+        beads[0].color === beads[1].color &&
+        beads[2].color === beads[1].color
+      ) {
+        console.log("beads need to be removed: ");
+        return true;
+      }
+      console.log("beads are not the same: ");
+
+      return false;
+    };
+
+    if (firstThreeBeadsAreTheSame(beads)) {
+      // remove all these beads
+      removeFromLeft();
+      removeFromLeft();
+      removeFromLeft();
+      setScore(() => score + 1)
+    }
+    const lastThreeBeadsAreTheSame = (beads) => {
+      if (
+        beads[beads.length - 1].color === beads[beads.length - 2].color &&
+        beads[beads.length - 3].color === beads[beads.length - 2].color
+      ) {
+        console.log("beads need to be removed: ");
+        return true;
+      }
+      console.log("beads are not the same: ");
+
+      return false;
+    };
+    if (lastThreeBeadsAreTheSame(beads)) {
+      // remove all these beads
+      removeFromRight();
+      removeFromRight();
+      removeFromRight();
+      setScore(() => score + 1)
+    }
+  }, [nextBeadColor, beads]);
+
+  const handleKeyDown = (e) => {
+    console.log(e.keycode);
+    console.log(e.key);
+  };
+
+  return (
+    <MainWrapper onKeyDown={(e) => handleKeyDown(e)}>
+      <h1>Array methods</h1>
+      <GameArea>
+        <Container>
+          <BeadHolder>
+            {beads?.map(({ id, color }) => (
+              <ColoredBead key={id} $color={color} />
+            ))}
+          </BeadHolder>
+        </Container>
+        <div style={{ margin: "0 auto" }}>
+          <h2>Information</h2>
+          <p>Current score: {score}</p>
+          <p>Next bead: {nextBeadColor}</p>
+        </div>
+        <ControlCenter>
+          <button onClick={() => addToLeft()}>Add Left</button>
+          <button onClick={() => addToRight()}>Add Right</button>
+          {/* <button onClick={() => removeFromLeft()}>RemoveLeft</button> */}
+          {/* <button onClick={() => removeFromRight()}>Remove Right</button> */}
+        </ControlCenter>
+      </GameArea>
+    </MainWrapper>
+  );
+};
