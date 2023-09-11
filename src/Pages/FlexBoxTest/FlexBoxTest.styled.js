d9247e1f2a4038ceaa113b401b22c94a5b24157f
Emil Nilsson <emil.nilsson@dataductus.se>
2023-09-11 17:08:24 +0200

    Add page for showcasing a flexbox trick

--- src/Pages/FlexBoxTest/FlexBoxTest.styled.js
+++ src/Pages/FlexBoxTest/FlexBoxTest.styled.js
@@ -1,13 +1,16 @@
 import { styled } from "styled-components";
 
+export const MyContainerWrapper = styled.div`
+  background-color: rgba(255, 0, 0, 0.3);
+  padding: 8px;
+`;
 export const MyContainer = styled.div`
   display: flex;
   gap: 10px;
-  /* width: 100px; */
   margin: 0 auto;
   width: 80vw;
   justify-content: center;
-`
+`;
 export const MyBox = styled.div`
   background-color: red;
 
@@ -21,4 +24,83 @@ export const MyBox = styled.div`
   &:last-child {
     margin-left: auto;
   }
-`
+`;
+export const MyH3 = styled.h3`
+  text-align: center;
+`;
+
+export const MyContainer2 = styled(MyContainer)`
+  display: flex;
+  gap: 10px;
+  margin: 0 auto;
+  width: 80vw;
+  justify-content: center;
+`;
+
+export const MyBox2 = styled(MyBox)`
+  &:first-child {
+    margin-right: unset;
+  }
+  &:last-child {
+    margin-left: unset;
+  }
+`;
+
+export const MyContainer3 = styled(MyContainer)`
+  display: flex;
+  gap: 10px;
+  margin: 0 auto;
+  width: 80vw;
+  justify-content: center;
+`;
+
+export const MyBox3 = styled(MyBox)`
+  background-color: red;
+
+  &:first-child {
+    margin-right: auto;
+  }
+
+  &:nth-child(3) {
+    margin-left: auto;
+  }
+
+  &:last-child {
+    margin-left: unset;
+  }
+`;
+
+export const MyContainer4 = styled(MyContainer)`
+  display: flex;
+  gap: 10px;
+  margin: 0 auto;
+  width: 80vw;
+  justify-content: center;
+`;
+
+export const MyBox4 = styled(MyBox)`
+  background-color: red;
+
+  &:first-child {
+    margin-right: auto;
+  }
+
+  &:last-child {
+    margin-right: auto;
+  }
+`;
+
+export const MyContainer5 = styled(MyContainer)`
+  display: flex;
+  gap: 10px;
+  margin: 0 auto;
+  width: 80vw;
+  justify-content: center;
+`;
+export const MyBox5 = styled(MyBox)`
+  background-color: red;
+
+  &:first-child {
+    margin-right: auto;
+  }
+`;
--- src/Pages/FlexBoxTest/FlexBoxTest.tsx
+++ src/Pages/FlexBoxTest/FlexBoxTest.tsx
@@ -1,37 +1,218 @@
 import React from "react";
 import { MainWrapper } from "../../Shared/Page.styled";
-import { MyBox, MyContainer } from "./FlexBoxTest.styled";
+import {
+  MyBox,
+  MyBox2,
+  MyBox3,
+  MyBox4,
+  MyBox5,
+  MyContainer,
+  MyContainer2,
+  MyContainer3,
+  MyContainer4,
+  MyContainer5,
+  MyContainerWrapper,
+  MyH3,
+} from "./FlexBoxTest.styled";
 
 export const FlexBoxTest = () => {
   return (
     <MainWrapper>
       <h1>Flexbox testing</h1>
-      <MyContainer>
-        <MyBox>
-          <pre>{`
+      <section>
+        <h2>Case 1</h2>
+        <MyContainerWrapper>
+          <MyH3>
+            <pre>{`
+justify-content: center;  
+            `}</pre>
+          </MyH3>
+          <Case1 />
+        </MyContainerWrapper>
+      </section>
+      <section>
+        <h2>Case 2</h2>
+        <MyContainerWrapper>
+          <MyH3>
+            <pre>{`
+justify-content: center;  
+            `}</pre>
+          </MyH3>
+          <Case2 />
+        </MyContainerWrapper>
+      </section>
+      <section>
+        <h2>Case 3</h2>
+        <MyContainerWrapper>
+          <MyH3 style={{ textAlign: "center" }}>
+            <pre>{`
+justify-content: center;  
+            `}</pre>
+          </MyH3>
+          <Case3 />
+        </MyContainerWrapper>
+      </section>
+      <section>
+        <h2>Case 4</h2>
+        <MyContainerWrapper>
+          <MyH3>
+            <pre>{`
+justify-content: center;  
+            `}</pre>
+          </MyH3>
+          <Case4 />
+        </MyContainerWrapper>
+      </section>
+      <section>
+        <h2>Case 5</h2>
+        <MyContainerWrapper>
+          <MyH3>
+            <pre>{`
+justify-content: center;  
+            `}</pre>
+          </MyH3>
+          <Case5 />
+        </MyContainerWrapper>
+      </section>
+    </MainWrapper>
+  );
+};
+
+const Case1 = () => (
+  <MyContainer>
+    <MyBox>
+      <pre>{`
 &:first-child {
   margin-right: auto;
 }
-          `}</pre>
-        </MyBox>
-        <MyBox>
-          <pre>{`
-justify-content: center;
-          `}</pre>
-        </MyBox>
-        <MyBox>
-          <pre>{`
-justify-content: center;
-          `}</pre>
-        </MyBox>
-        <MyBox>
-          <pre>{`
+`}</pre>
+    </MyBox>
+    <MyBox>
+      <pre>{`
+
+`}</pre>
+    </MyBox>
+    <MyBox>
+      <pre>{`
+
+`}</pre>
+    </MyBox>
+    <MyBox>
+      <pre>{`
 &:last-child {
   margin-left: auto;
 }
 `}</pre>
-        </MyBox>
-      </MyContainer>
-    </MainWrapper>
-  );
-};
+    </MyBox>
+  </MyContainer>
+);
+
+const Case2 = () => (
+  <MyContainer2>
+    <MyBox2>
+      <pre>{`
+
+`}</pre>
+    </MyBox2>
+    <MyBox2>
+      <pre>{`
+
+`}</pre>
+    </MyBox2>
+    <MyBox2>
+      <pre>{`
+
+`}</pre>
+    </MyBox2>
+    <MyBox2>
+      <pre>{`
+
+`}</pre>
+    </MyBox2>
+  </MyContainer2>
+);
+
+const Case3 = () => (
+  <MyContainer3>
+    <MyBox3>
+      <pre>{`
+&:first-child {
+  margin-right: auto;
+}
+`}</pre>
+    </MyBox3>
+    <MyBox3>
+      <pre>{`
+
+`}</pre>
+    </MyBox3>
+    <MyBox3>
+      <pre>{`
+&:nth-child(3) {
+  margin-left: auto;
+}
+`}</pre>
+    </MyBox3>
+    <MyBox3>
+      <pre>{`
+
+`}</pre>
+    </MyBox3>
+  </MyContainer3>
+);
+
+const Case4 = () => (
+  <MyContainer4>
+    <MyBox4>
+      <pre>{`
+&:first-child {
+  margin-right: auto;
+}
+`}</pre>
+    </MyBox4>
+    <MyBox4>
+      <pre>{`
+
+`}</pre>
+    </MyBox4>
+    <MyBox4>
+      <pre>{`
+
+`}</pre>
+    </MyBox4>
+    <MyBox4>
+      <pre>{`
+&:last-child {
+  margin-right: auto;
+}
+`}</pre>
+    </MyBox4>
+  </MyContainer4>
+);
+
+const Case5 = () => (
+  <MyContainer5>
+    <MyBox5>
+      <pre>{`
+&:first-child {
+  margin-right: auto;
+}
+`}</pre>
+    </MyBox5>
+    <MyBox5>
+      <pre>{`
+
+`}</pre>
+    </MyBox5>
+    <MyBox5>
+      <pre>{`
+
+`}</pre>
+    </MyBox5>
+    <MyBox5>
+      <pre>{`
+
+`}</pre>
+    </MyBox5>
+  </MyContainer5>
+);
