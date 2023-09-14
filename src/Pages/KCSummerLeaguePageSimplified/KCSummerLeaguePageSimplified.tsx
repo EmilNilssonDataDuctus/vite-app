18c0568bb97f63de00e72c0ddb2340fefd38a004
Emil Nilsson <emil.nilsson@dataductus.se>
2023-09-14 11:17:13 +0200

    Add input for adding and deleting users

--- src/Pages/KCSummerLeaguePageSimplified/KCSummerLeaguePageSimplified.tsx
+++ src/Pages/KCSummerLeaguePageSimplified/KCSummerLeaguePageSimplified.tsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { MainWrapper } from "../../Shared/Page.styled";
 
 export const KCSummerLeaguePageSimplified = () => {
+  const [climbersData, setClimbersData] = useState<Array<string>>([]);
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setClimbersData([...climbersData, inputValue]);
+    setInputValue("");
   };
-  const handleTextInputChange = () => {};
+
+  const handleDelete = (target) => {
+    setClimbersData([...climbersData].filter((climber) => climber !== target));
+  };
+
+  const handleTextInputChange = (e) => {
+    setInputValue(e.target.value);
+    console.log(e.target.value);
+    console.log(typeof e.target.value);
+  };
 
   return (
     <MainWrapper>
       <h1>KCSummerLeagueTracker</h1>
       <div>
         <h2>Table of climbers</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleTextInputChange} />
-          <input type="submit" />
-        </form>
+        <section>
+          <h3>Add a new climber</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleTextInputChange}
+            />
+            <input type="submit" />
+          </form>
+        </section>
+        <section>
+          <h3>Current standings</h3>
+          <ul>
+            {climbersData?.map((climber) => (
+              <li key={climber}>
+                climber: {climber}{" "}
+                <button onClick={() => handleDelete(climber)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
     </MainWrapper>
   );
